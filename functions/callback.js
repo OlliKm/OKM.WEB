c889c5e123bbcb9df5d8a329736e2eb646c5c20f
ollikm <mrmccamley@gmail.com>
2026-07-31 21:13:17 +0100

    v5

---
+++ functions/callback.js
@@ -0,0 +1,62 @@
+function renderBody(status, content) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head><meta charset="utf-8"><title>Auth Result</title></head>
+<body>
+<script>
+  const receiveMessage = (message) => {
+    window.opener.postMessage(
+      'authorization:github:${status}:${JSON.stringify(content)}',
+      message.origin
+    );
+    window.removeEventListener("message", receiveMessage, false);
+  }
+  window.addEventListener("message", receiveMessage, false);
+  window.opener.postMessage("authorizing:github", "*");
+</script>
+</body>
+</html>`;
+}
+
+export async function onRequest(context) {
+  const { request, env } = context;
+  const client_id = env.GITHUB_CLIENT_ID;
+  const client_secret = env.GITHUB_CLIENT_SECRET;
+
+  try {
+    const url = new URL(request.url);
+    const code = url.searchParams.get('code');
+
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'user-agent': 'cloudflare-pages-github-oauth',
+        'accept': 'application/json',
+      },
+      body: JSON.stringify({ client_id, client_secret, code }),
+    });
+
+    const result = await response.json();
+
+    if (result.error) {
+      return new Response(renderBody('error', result), {
+        headers: { 'content-type': 'text/html;charset=UTF-8' },
+        status: 401,
+      });
+    }
+
+    const token = result.access_token;
+    const responseBody = renderBody('success', { token, provider: 'github' });
+
+    return new Response(responseBody, {
+      headers: { 'content-type': 'text/html;charset=UTF-8' },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(error.message, {
+      headers: { 'content-type': 'text/html;charset=UTF-8' },
+      status: 500,
+    });
+  }
+}
